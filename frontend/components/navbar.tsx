b11573466590b5df2cd0158dd6e4cc4dcc0531b2
Singh <h.singh8149@gmail.com>
2023-12-28 19:17:06 +0100

    added bottom border to the nav to make it cool more cool

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -8,7 +8,7 @@ import CLogoutButton from './CLogout';
 
 export default function NavBar() {
   return (
-    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6 border-b-2 border-b-black dark:border-b-white ">
       <Sheet>
         <SheetTrigger asChild>
           <Button className="lg:hidden" size="icon" variant="outline">
