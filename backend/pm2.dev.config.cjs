fe7f59d5fc04f223b69edadaae393e8268d99c38
Singh <h.singh8149@gmail.com>
2023-12-30 17:29:05 +0100

    maybe fixed the pm2 files for bun

--- backend/pm2.dev.config.cjs
+++ backend/pm2.dev.config.cjs
@@ -1,19 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'qat_prod_frontend', // Name of the TypeScript application
-      script: 'run', // Script to be executed
-      args: './out/index.js', // 'start' for running in production mode
+      name: 'qat_dev_backend', // Name of the TypeScript application
+      script: './out/index.js', // Script to be executed
+      args: '', // 'start' for running in production mode
       watch: false, // Enable watching of file changes (set to false in production)
       env: {
         NODE_ENV: 'production', // Set NODE_ENV for production
         PORT: 4002, // Application's port
       },
-      interpreter: "~/.bun/bin/bun",
-      exec_mode: 'cluster', // Enable cluster mode for load balancing
-      instances: 'max', // Use 'max' to utilize all available cores
+      interpreter: 'bun',
+      // exec_mode: 'cluster', // Enable cluster mode for load balancing
+      // instances: 'max', // Use 'max' to utilize all available cores
       autorestart: true, // Automatically restart if the app crashes
-      max_memory_restart: '2G', // Restart if memory limit is reached
+      max_memory_restart: '4G', // Restart if memory limit is reached
     },
   ],
 };
