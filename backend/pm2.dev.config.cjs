a27d0edbc2f2a7aaa4bc090f64e0e78e42086da7
Singh <h.singh8149@gmail.com>
2023-12-30 17:38:34 +0100

    fix 2 pm2 files

--- backend/pm2.dev.config.cjs
+++ backend/pm2.dev.config.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'qat_prod_backend', // Name of the TypeScript application
+      name: 'qat_dev_backend', // Name of the TypeScript application
       script: '~./.bun/bin/bun', // Script to be executed
       args: './out/index.js', // 'start' for running in production mode
       watch: false, // Enable watching of file changes (set to false in production)
